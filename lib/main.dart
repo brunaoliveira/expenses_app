e3c468c54e956813c0dee5ef5ebe02a4e89ba875
brunaoliveira <brunaol678@gmail.com>
2020-12-27 17:17:59 -0300

    allows scroll, preventing screen overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,21 @@ class MyHomePage extends StatelessWidget {
         title: Text('Personal Expenses'),
         backgroundColor: Colors.purple[500],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('Graph'),
-              color: Colors.purple[50],
-              elevation: 5,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('Graph'),
+                color: Colors.purple[50],
+                elevation: 5,
+              ),
             ),
-          ),
-          TransactionUser(),
-        ],
+            TransactionUser(),
+          ],
+        ),
       ),
     );
   }
